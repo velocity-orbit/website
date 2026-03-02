45b1f653669a6bcf7b3dd6ea3ba301d2e045ffb8
Suyash2304 <suyashborikar23@gmail.com>
2026-03-03 00:30:07 +0530

    fix: bug fix in hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,47 +17,8 @@ export default function Hero() {
         }}
       >
         <ShaderGradient
-          animate="on"
-          bgColor1="#000000"
-          bgColor2="#000000"
-          brightness={1}
-          cAzimuthAngle={180}
-          cDistance={3.9}
-          cPolarAngle={115}
-          cameraZoom={1}
-          color1="#5606ff"
-          color2="#fe8989"
-          color3="#000000"
-          destination="onCanvas"
-          embedMode="off"
-          envPreset="city"
-          format="gif"
-          fov={45}
-          frameRate={10}
-          gizmoHelper="hide"
-          grain="off"
-          lightType="3d"
-          pixelDensity={1}
-          positionX={-0.5}
-          positionY={0.1}
-          positionZ={0}
-          range="disabled"
-          rangeEnd={40}
-          rangeStart={0}
-          reflection={0.1}
-          rotationX={0}
-          rotationY={0}
-          rotationZ={235}
-          shader="defaults"
-          type="waterPlane"
-          uAmplitude={0}
-          uDensity={1.1}
-          uFrequency={5.5}
-          uSpeed={0.1}
-          uStrength={2.4}
-          uTime={0.2}
-          wireframe={false}
-          zoomOut={false}
+          control="query"
+          urlString="https://shadergradient.co/customize?animate=on&axesHelper=on&bgColor1=%23000000&bgColor2=%23000000&brightness=1.1&cAzimuthAngle=180&cDistance=3.9&cPolarAngle=115&cameraZoom=1&color1=%235606ff&color2=%23fe8989&color3=%23000000&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=-0.5&positionY=0.1&positionZ=0&range=disabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=235&shader=defaults&type=waterPlane&uAmplitude=0&uDensity=1.1&uFrequency=5.5&uSpeed=0.1&uStrength=2.4&uTime=0.2&wireframe=false"
         />
       </ShaderGradientCanvas>
       <div className="h-screen w-full relative">
